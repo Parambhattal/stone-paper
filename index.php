e000bc711df54c47d32809f5f8d75dcdee0bdb04
Param Bhattal <paramsingh081021@gmail.com>
2025-11-08 23:57:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Rima Hazra e5c160fe - Rock Paper Scissors</title>
+</head>
+<body>
+  <h1>Welcome to Rock Paper Scissors</h1>
+  <p><a href="login.php">Please Log In</a></p>
+  <p>
+    Attempt to go to 
+    <a href="game.php">game.php</a> 
+    without logging in - it should fail with an error message.
+  </p>
+</body>
+</html>
